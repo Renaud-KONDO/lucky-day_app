9f84cb81a1957fb4f8c617115a908f1263670525
essossolam renaud kondo <renaudkondo2@gmail.com>
2026-02-27 19:23:40 +0000

    adapted the gradel for flutter_local notifications to meet java and gradel versions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,16 +3,18 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.luckyday.lucky_day"
+    namespace = "com.auraphine.lucky_day"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true 
     }
 
     kotlinOptions {
@@ -21,7 +23,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.luckyday.lucky_day"
+        applicationId = "com.auraphine.lucky_day"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -39,6 +41,13 @@ android {
     }
 }
 
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")  
+}
+
+
 flutter {
     source = "../.."
 }
+
+//apply(plugin = "com.google.gms.google-services")
